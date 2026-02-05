d082c56e358fff09ee12cae9fdd2bb62b7b51049
clusterfluffed <120131234+clusterfluffed@users.noreply.github.com>
2026-02-04 22:15:04 -0500

    Update page.tsx

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -137,10 +137,16 @@ export default function SignInPage() {
           </Link>
         </div>
 
-        {/* Help Text */}
-        <div className="text-center mt-4">
-          <p className="text-xs text-gray-500">
-            By signing in, you agree to our Terms of Service and Privacy Policy
+          {/* Terms */}
+          <p className="text-xs text-center text-gray-500">
+            By creating an account, you agree to our{' '}
+            <Link href="/legal/terms" target="_blank" className="text-blue-600 hover:text-blue-800 underline">
+              Terms of Service
+            </Link>
+            {' '}and{' '}
+            <Link href="/legal/privacy" target="_blank" className="text-blue-600 hover:text-blue-800 underline">
+              Privacy Policy
+            </Link>
           </p>
         </div>
       </div>
